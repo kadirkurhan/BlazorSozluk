889236c9376cc005acec9a7aa073e3441fc39a24
kadirkurhan <kadirkurhan42@hotmail.com>
2022-06-21 20:05:54 +0300

    entityframework configurations

---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/EntityConfigurations/EntryComment/EntryCommentVoteEntityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using BlazorSozluk.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Infrastructure.Persistence.EntityConfigurations.EntryComment;
+
+public class EntryCommentVoteEntityConfiguration:BaseEntityConfiguration<Api.Domain.Models.EntryCommentVote>
+{
+    public override void Configure(EntityTypeBuilder<Api.Domain.Models.EntryCommentVote> builder)
+    {
+        base.Configure(builder);
+
+        builder.ToTable("entrycommentvote", BlazorSozlukContext.DEFAULT_SCHEMA);
+
+        builder.HasOne(i => i.EntryComment).WithMany(i => i.EntryCommentVotes).HasForeignKey(i => i.EntryCommentId);
+    }
+}
