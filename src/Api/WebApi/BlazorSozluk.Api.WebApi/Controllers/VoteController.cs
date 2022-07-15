c61729fd49bb28d359456abe32e8cb98b474038a
kadirkurhan <kadirkurhan42@hotmail.com>
2022-07-15 16:11:01 +0300

    feat: vote controller added

--- src/Api/WebApi/BlazorSozluk.Api.WebApi/Controllers/VoteController.cs
+++ src/Api/WebApi/BlazorSozluk.Api.WebApi/Controllers/VoteController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BlazorSozluk.Api.Application.Features.Commands.Entry.DeleteVote;
+using BlazorSozluk.Api.Application.Features.Commands.EntryComment.DeleteVote;
+using BlazorSozluk.Common.ViewModels;
+using BlazorSozluk.Common.ViewModels.RequestModels;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorSozluk.Api.WebApi.Controllers
@@ -7,5 +12,43 @@ namespace BlazorSozluk.Api.WebApi.Controllers
     [ApiController]
     public class VoteController : CustomBaseController
     {
+        private readonly IMediator _mediator;
+
+        public VoteController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("Entry/{entryId}")]
+        public async Task<IActionResult> CreateEntryVote(Guid entryId,VoteType voteType = VoteType.UpVote)
+        {
+            var result = await _mediator.Send(new CreateEntryVoteCommand(entryId,voteType,UserId));
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("EntryComment/{entryCommentId}")]
+        public async Task<IActionResult> CreateEntryCommentVote(Guid entryCommentId, VoteType voteType = VoteType.UpVote)
+        {
+            var result = await _mediator.Send(new CreateEntryCommentVoteCommand(entryCommentId, voteType, UserId));
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("DeleteEntryVote/{entryId}")]
+        public async Task<IActionResult> DeleteEntryVote(Guid entryId)
+        {
+            await _mediator.Send(new DeleteEntryVoteCommand(entryId,UserId));
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("DeleteEntryCommentVote/{entryId}")]
+        public async Task<IActionResult> DeleteEntryCommentVote(Guid entryId)
+        {
+            await _mediator.Send(new DeleteEntryCommentVoteCommand(entryId, UserId));
+            return Ok();
+        }
     }
 }
